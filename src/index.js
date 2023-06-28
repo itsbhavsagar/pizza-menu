1e3d1bae903ef54a2d2440f928e12bbb027b5ea3
Bhavsagar <bhavsagar@protonmail.com>
2023-06-28 23:31:54 +0530

    ternary op

--- src/index.js
+++ src/index.js
@@ -7,42 +7,42 @@ const pizzaData = [
     name: "Focaccia",
     ingredients: "Bread with italian olive oil and rosemary",
     price: 6,
-    photoName: "/pizzas/focaccia.jpg",
+    photoName: "pizza/focaccia.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Margherita",
     ingredients: "Tomato and mozarella",
     price: 10,
-    photoName: "/pizzas/margherita.jpg",
+    photoName: "pizza/margherita.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Spinaci",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
-    photoName: "/pizzas/spinaci.jpg",
+    photoName: "pizza/spinaci.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Funghi",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
-    photoName: "/pizzas/funghi.jpg",
+    photoName: "pizza/funghi.jpg",
     soldOut: false,
   },
   {
     name: "Pizza Salamino",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
-    photoName: "/pizzas/salamino.jpg",
+    photoName: "pizza/salamino.jpg",
     soldOut: true,
   },
   {
     name: "Pizza Prosciutto",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
-    photoName: "/pizzas/prosciutto.jpg",
+    photoName: "pizza/prosciutto.jpg",
     soldOut: false,
   },
 ];
@@ -66,14 +66,20 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -110,12 +116,17 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  console.log(isOpen);
+  // if (hour >= openHour && hour <= closeHour) alert(" We're currently open!");
   // else alert("Sorry we're closed!");
-
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently Open!
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
